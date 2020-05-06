d6e2a2ac58a61acd28441348cdb22a29dc83ed8d
Dawna Backmeier <karanacesin@yahoo.com>
2020-05-06 13:55:40 -0400

    modified

--- js/index.js
+++ js/index.js
@@ -1,6 +1,38 @@
+function get(selector) {
+    return document.querySelector(selector)
+  }
+
+const navlink = get('.nav-link');
+const nav = get('.nav');
+
+
 const navcolor = (event) =>{
-    nav.a.style.color = "lightblue";
+    navlink.style.color = "skyblue";
 }
 
-nav-link.addEventListener("mouseover", navcolor);
+navlink.addEventListener('mouseover', navcolor);
+
+const logo = get('.logo-heading');
+
+const logocolor = (event) =>{
+    logo.style.color = 'yellow';
+}
+
+logo.addEventListener('mouseover', logocolor);
+
+
+const adventure = get('.img-content');
+let scale = 1;
+function zoom(event) {
+    event.preventDefault();
+  
+    scale += event.deltaY * -0.01;
+  
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    adventure.style.transform = `scale(${scale})`;
+  }
+  
+  
+ adventure.addEventListener('wheel', zoom);
 
